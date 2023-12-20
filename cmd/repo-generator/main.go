7c19e9c81bc46e765b409fd88e00754eadf83e80
Darien Hernandez <dohernandez@gmail.com>
2023-12-20 13:02:22 +0100

    feat: Add command repo-generate

--- cmd/repo-generator/main.go
+++ cmd/repo-generator/main.go
@@ -1,20 +1,122 @@
 package main
 
 import (
-	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
-	_ "github.com/ethereum/go-ethereum/common"
-	_ "github.com/google/uuid"
-	_ "github.com/lib/pq"
+	"github.com/spf13/cobra"
+
+	generator "github.com/dohernandez/repo-generator"
 )
 
 var (
-	outputPath = flag.String("output", "", "Path of the output file")
-	model      = flag.String("model", "", "Name of struct to generate repo for")
-	tag        = flag.String("tag", "", "Tag to use for the generated repo")
-	//tag        = flag.StringArray("tag", "", "Tag to use for the generated repo")
+	srcpkg string
+	output string
+	model  string
+	create bool
+	insert bool
+	update bool
+	del    bool
 )
 
-func main() {
+var rootCmd = &cobra.Command{
+	Use:     "repo-generator",
+	Short:   "A CLI repo-generator",
+	Long:    "Generate repo objects for your Golang model struct\n",
+	Version: "v0.1.0", // TODO: read from version.txt
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Starting %s version=%s\n", cmd.Use, cmd.Version)
 
+		var (
+			options  []generator.Option
+			optFlags []string
+		)
+		// Your logic for handling the selected functionalities
+		if create {
+			optFlags = append(optFlags, "--create")
+		}
+
+		if insert {
+			optFlags = append(optFlags, "--insert")
+		}
+
+		if update {
+			optFlags = append(optFlags, "--update")
+		}
+
+		if del {
+			optFlags = append(optFlags, "--delete")
+		}
+
+		if output == "" {
+			output = generateGenPath(srcpkg)
+		}
+
+		flags := strings.Join(optFlags, " ")
+
+		// Additional logic for your application based on the provided arguments
+		fmt.Printf("Generating mock model=%s qualified-name=%s %s version=%s\n",
+			model,
+			srcpkg,
+			flags,
+			cmd.Version,
+		)
+
+		if err := generator.Generate(
+			srcpkg,
+			output,
+			model,
+			options...,
+		); err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("repo file generated!")
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&srcpkg, "srcpkg", "", "Source pkg to search for struct (required)")
+	rootCmd.PersistentFlags().StringVar(&output, "output", "", "Path of the output file (required)")
+	rootCmd.PersistentFlags().StringVar(&model, "model", "", "Name of the struct to generate repo for (required)")
+	rootCmd.PersistentFlags().BoolVar(&create, "create", false, "Enable repo create functionality")
+	rootCmd.PersistentFlags().BoolVar(&insert, "insert", false, "Enable repo insert functionality")
+	rootCmd.PersistentFlags().BoolVar(&update, "update", false, "Enable repo update functionality")
+	rootCmd.PersistentFlags().BoolVar(&del, "delete", false, "Enable repo delete functionality")
+
+	rootCmd.AddCommand(completionCmd)
+}
+
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generate the autocompletion script for the specified shell",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Implementation for the completion command
+		fmt.Println("Generating autocompletion script...")
+	},
+}
+
+func generateGenPath(originalPath string) string {
+	// Extract the directory from the original path
+	path := filepath.Dir(originalPath)
+
+	// Extract the extension from the original path
+	ext := filepath.Ext(originalPath)
+
+	// Extract the filename without the extension
+	filename := strings.TrimSuffix(filepath.Base(originalPath), ext)
+
+	genFilename := fmt.Sprintf("%s_gen%s", filename, ext)
+
+	// Append "_gen" to the filename and join with the directory
+	return filepath.Join(path, genFilename)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,12 @@ go 1.19
 require (
 	github.com/cenkalti/backoff/v4 v4.1.3
 	github.com/dohernandez/errors v0.0.0-20231017211553-9f8df5e0169a
-	github.com/ethereum/go-ethereum v1.12.0
 	github.com/google/uuid v1.3.0
 	github.com/iancoleman/strcase v0.2.0
 	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa
 	github.com/jackc/pgx/v5 v5.2.0
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/lib/pq v1.10.9
+	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.8.4
 	mvdan.cc/gofumpt v0.5.0
 )
@@ -19,12 +18,13 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	golang.org/x/tools v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect

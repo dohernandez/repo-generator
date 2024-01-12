174900d185ec6a6d6c3934aacc5f4fb7a18cf6e6
Darien Hernandez <dohernandez@gmail.com>
2024-01-12 17:31:12 +0100

    patch: main.mk

--- internal/makefiles/main.mk
+++ internal/makefiles/main.mk
@@ -2,8 +2,5 @@ GO ?= go
 
 PWD = $(shell pwd)
 
-# Detecting GOPATH and removing trailing "/" if any
-GOPATH = $(realpath $(shell $(GO) env GOPATH))
-
 TEST_REPO_GENERATOR_DEVGO_PATH ?= $(PWD)/internal/makefiles
 TEST_REPO_GENERATOR_DEVGO_SCRIPTS ?= $(TEST_REPO_GENERATOR_DEVGO_PATH)
